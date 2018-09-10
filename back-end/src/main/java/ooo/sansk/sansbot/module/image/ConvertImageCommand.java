be7b53372e379b7f2a8555e5ead4a48c4ebe8056
Sander Knauff <sanderknauff@hotmail.com>
2018-09-10 15:51:14 +0200

    Allow for multiple ImageFilter implementations

--- back-end/src/main/java/ooo/sansk/sansbot/module/image/ConvertImageCommand.java
+++ back-end/src/main/java/ooo/sansk/sansbot/module/image/ConvertImageCommand.java
@@ -2,10 +2,13 @@ package ooo.sansk.sansbot.module.image;
 
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.requests.Route;
 import net.dv8tion.jda.core.requests.restaction.MessageAction;
 import nl.imine.vaccine.annotation.Component;
 import ooo.sansk.sansbot.command.ChatCommand;
 import ooo.sansk.sansbot.command.ChatCommandHandler;
+import ooo.sansk.sansbot.module.image.filter.FilterType;
+import ooo.sansk.sansbot.module.image.filter.ImageFilter;
 import ooo.sansk.sansbot.module.image.filter.InvertColorImageFilter;
 
 import javax.imageio.ImageIO;
@@ -31,25 +34,49 @@ public class ConvertImageCommand extends ChatCommand {
 
     @Override
     public void handle(MessageReceivedEvent messageReceivedEvent) {
-        Optional<BufferedImage> oBufferedImage = messageReceivedEvent.getMessage().getAttachments().stream()
+        String[] args = messageReceivedEvent.getMessage().getContentRaw().split(" ");
+        if(args.length < 2) {
+            reply(messageReceivedEvent.getChannel(), "Ik heb geen idee wat je van me mot, %s...");
+            return;
+        }
+        if(args.length > 3) {
+            reply(messageReceivedEvent.getChannel(), "Ja ehh, niet zoveel tegelijk hè, %s");
+            return;
+        }
+
+        Optional<ImageFilter> oFilter = FilterType.getFilter(args[1]).map(FilterType::getFilter);
+        if (!oFilter.isPresent()) {
+            reply(messageReceivedEvent.getChannel(), "Ik kan een hoop, maar dat nou net weer niet. %s");
+            return;
+        }
+
+        messageReceivedEvent.getChannel().getHistory().retrievePast(10).queue(messages -> {
+            Optional<BufferedImage> oBufferedImage = getFirstLoadedMessageWithImageAttachment(messages);
+
+            if (oBufferedImage.isPresent()) {
+                ImageResult imageResult = oFilter.get().doFilter(oBufferedImage.get());
+                if (imageResult.getImageData().length < MAX_FILE_SIZE_BYTES) {
+                    MessageAction messageAction = messageReceivedEvent.getChannel().sendFile(imageResult.getImageData(), "output." + imageResult.getImageType());
+                    if (messageReceivedEvent.getChannel().getType().isGuild()) {
+                        messageAction = messageAction.content(messageReceivedEvent.getMember().getAsMention());
+                    }
+                    messageAction.submit();
+                } else {
+                    reply(messageReceivedEvent.getChannel(), String.format("Oh nee! deze is te groot voor mij, %s :confounded:", messageReceivedEvent.getAuthor().getAsMention()));
+                }
+            } else {
+                reply(messageReceivedEvent.getChannel(), String.format("Ge snappe het volgensmij niet helemaal hè, %s. Ge motten wel een plaatje erbij uploaden hè? :angry:", messageReceivedEvent.getAuthor().getAsMention()));
+            }
+        });
+    }
+
+    private Optional<BufferedImage> getFirstLoadedMessageWithImageAttachment(List<Message> messages) {
+        return messages
+                .stream()
+                .flatMap(message -> message.getAttachments().stream())
                 .filter(Message.Attachment::isImage)
                 .findFirst()
                 .map(this::downloadImageFromAttachment);
-
-        if (oBufferedImage.isPresent()) {
-            ImageResult imageResult = new InvertColorImageFilter().doFilter(oBufferedImage.get());
-            if (imageResult.getImageData().length < MAX_FILE_SIZE_BYTES) {
-                MessageAction messageAction = messageReceivedEvent.getChannel().sendFile(imageResult.getImageData(), "inverted.png");
-                if (messageReceivedEvent.getChannel().getType().isGuild()) {
-                    messageAction = messageAction.content(messageReceivedEvent.getMember().getAsMention());
-                }
-                messageAction.submit();
-            } else {
-                reply(messageReceivedEvent.getChannel(), String.format("Oh nee! deze is te groot voor mij, %s :confounded:", messageReceivedEvent.getAuthor().getAsMention()));
-            }
-        } else {
-            reply(messageReceivedEvent.getChannel(), String.format("Ge snappe het volgensmij niet helemaal hè, %s. Ge motten wel een plaatje erbij uploaden hè? :angry:", messageReceivedEvent.getAuthor().getAsMention()));
-        }
     }
 
     private BufferedImage downloadImageFromAttachment(Message.Attachment attachment) {
---
+++ back-end/src/main/java/ooo/sansk/sansbot/module/image/filter/FilterType.java
@@ -0,0 +1,35 @@
+package ooo.sansk.sansbot.module.image.filter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public enum FilterType {
+
+    INVERT(new InvertColorImageFilter(), Arrays.asList("inverted", "invert", "i")),
+    BW(new BlackWhiteColorImageFilter(), Arrays.asList("blackwhite", "bw"));
+
+    private final ImageFilter filter;
+    private final List<String> aliases;
+
+    FilterType(ImageFilter filter, List<String> aliases) {
+        this.filter = filter;
+        this.aliases = aliases;
+    }
+
+    public ImageFilter getFilter() {
+        return filter;
+    }
+
+    public boolean hasAlias(String alias) {
+        return aliases.stream().anyMatch(alias::equalsIgnoreCase);
+    }
+
+    public static Optional<FilterType> getFilter(String name) {
+        return Stream.of(FilterType.values())
+                .filter(filterType -> filterType.hasAlias(name))
+                .findFirst();
+    }
+
+}
